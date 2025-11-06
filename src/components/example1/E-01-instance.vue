52b59c017783fa40e42a72045c0909241702af1c
jsb <tpqls1703@gmail.com>
2025-11-07 02:09:19 +0900

    initial commit

---
+++ src/components/example1/E-01-instance.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>Hello, {{ message }}</div>
+</template>
+
+<script>
+export default {
+  name: "E01Instance",
+  data() {
+    return {
+      message: "Vue!"
+    };
+  }
+};
+</script>
+
+<style scoped>
+div {
+  color: blue;
+  font-size: 20px;
+}
+</style>
